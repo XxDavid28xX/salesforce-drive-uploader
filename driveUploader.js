e3569dd5af89e7d4a73b344a5fd8ab2e0354c00e
XxDavid28xX <51222776+XxDavid28xX@users.noreply.github.com>
2025-06-18 15:52:51 -0600

    Update driveUploader.js

--- driveUploader.js
+++ driveUploader.js
@@ -17,7 +17,7 @@ const oauth2Client = new google.auth.OAuth2(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
   process.env.REDIRECT_URI
-);\n
+);
 oauth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 const drive = google.drive({ version: 'v3', auth: oauth2Client });
 
